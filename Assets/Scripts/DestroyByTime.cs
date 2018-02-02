25975c4009d2c7a4a2919878f5ac5bfb52e3e581
uriahrokach <uriahrokach@gmail.com>
2018-02-02 09:44:26 +0200

    Fixed camera settings in maze1-3D

--- Assets/Scripts/DestroyByTime.cs
+++ Assets/Scripts/DestroyByTime.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByTime : MonoBehaviour {
-
     public float timeUntilKilled = 100f;
 	
 	// Update is called once per frame
